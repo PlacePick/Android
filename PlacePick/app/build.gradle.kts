9266aa12897947942f07b793673d91f9f01022b8
JunYeong Kim <96372487+JunYeong0314@users.noreply.github.com>
2023-10-24 12:03:31 +0900

    [Refactor] 버전수정

--- PlacePick/app/build.gradle.kts
+++ PlacePick/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.kauproject.placepick"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.kauproject.placepick"
-        minSdk = 24
-        targetSdk = 33
+        minSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
